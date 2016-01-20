68a539658ca8d8f8e5b7937042aafe967d79032d
EliasL <elias.lundheim@gmail.com>
2016-01-20 20:04:35 +0100

    The main files
    Note that the large number reverse calculator does not compress all of
    it's numbers.

--- Reverse calculator.py
+++ Reverse calculator.py
@@ -1,4 +1,6 @@
 import math
+import itertools
+
 
 def square(x):
     return int(x) * int(x)
@@ -19,7 +21,7 @@ def findD(n, number, d):
     return n, number
 
 
-def compacter(number):
+def compacter1(number):
     sofar=[]
     for i in number:
         if i != 9:
@@ -40,7 +42,40 @@ def compacter(number):
     return number
 
 
-def reverse(n):
+def testing(number,list):
+    for i in list:
+        a = happy(i)
+
+
+def switch(number, old, new):
+    old = list(map(int, str(old)))
+    new = map(int, str(new))
+    for i in number:
+        if i in old:
+            number.remove(i)
+            old.remove(i)
+    for i in new:
+        number.append(i)
+    number.sort()
+    return number
+
+
+def compacter2(number):
+    possible = []
+    for L in range(0, len(number)):
+      for subset in itertools.combinations(number, L+1):
+        a = list(subset)
+        a = int(''.join(map(str, a)))
+        for i in range(0, 101):
+            if happy(a) == happy(i) and a != i and a > i:
+                print(str(a) + " is the same as " + str(i))
+                #possible.append(switch(number, a, i))
+    #print(possible)
+
+    return number
+
+
+def reverse(n, deep_compressing):
     i = 0
     number = []
     n, number = findD(n, number, 9)
@@ -53,13 +88,16 @@ def reverse(n):
     n, number = findD(n, number, 2)
     n, number = findD(n, number, 1)
     number = number[::-1]
-    if 9 in number and number[0] != 9:
-        number = compacter(number)
+    print(number)
+    if deep_compressing:
+        number = compacter2(number)
+    else:
+        number = compacter1(number)
     return int(''.join(map(str, number)))
 
 
 
 while True:
     n = int(input("Enter number: "))
-    n = reverse(n)
+    n = reverse(n, True)
     print(n)
